b4e9de05d2a9efc87793d98c609e6e42f1d82d2f
Dzmitry Bahdanovich <d.bahdanovich@andersenlab.com>
2023-10-10 17:22:55 +0200

    Upload demo project

---
+++ migrations/Version20231010151937.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231010151937 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE author_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE author (id INT NOT NULL, name VARCHAR(255) NOT NULL, date_of_birth TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, bio TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE book (id INT NOT NULL, author_id INT NOT NULL, title VARCHAR(255) NOT NULL, synopsis TEXT NOT NULL, release_year VARCHAR(4) NOT NULL, genre VARCHAR(255) NOT NULL, average_rating INT NOT NULL, copies_sold INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_CBE5A331F675F31B ON book (author_id)');
+        $this->addSql('ALTER TABLE book ADD CONSTRAINT FK_CBE5A331F675F31B FOREIGN KEY (author_id) REFERENCES author (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE author_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
+        $this->addSql('ALTER TABLE book DROP CONSTRAINT FK_CBE5A331F675F31B');
+        $this->addSql('DROP TABLE author');
+        $this->addSql('DROP TABLE book');
+    }
+}
